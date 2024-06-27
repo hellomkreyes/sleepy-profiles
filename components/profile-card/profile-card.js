8278e04788ecd9a20a19e4307c90bc92110e8585
Corey Sheldrick <corey@endy.com>
2024-06-27 15:17:23 -0400

    only one profile card

--- components/profile-card/profile-card.js
+++ components/profile-card/profile-card.js
@@ -18,36 +18,6 @@ const ProfileCard = () => {
 					</div>
 				</div>
 			</label>
-			<label className={styles.cardWrap}>
-				<input type="checkbox" className={styles.flipcard} />
-				<div className={styles.card}>
-					<div className={`${styles.front} ${styles.cardFace}`}>
-						<img
-							src="https://images.unsplash.com/photo-1531518326825-96490ddf2a89?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-							alt=""
-							className={styles.cardPhoto}
-						/>
-					</div>
-					<div className={`${styles.back} ${styles.cardFace}`}>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-					</div>
-				</div>
-			</label>
-			<label className={styles.cardWrap}>
-				<input type="checkbox" className={styles.flipcard} />
-				<div className={styles.card}>
-					<div className={`${styles.front} ${styles.cardFace}`}>
-						<img
-							src="https://images.unsplash.com/photo-1529911194209-8578109840df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-							alt=""
-							className={styles.cardPhoto}
-						/>
-					</div>
-					<div className={`${styles.back} ${styles.cardFace}`}>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-					</div>
-				</div>
-			</label>
 		</div>
 	);
 };
